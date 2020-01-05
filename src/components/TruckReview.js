7bd611479ffa236c026306e367c094807c8ae059
Jose <Jrivera6869@yahoo.com>
2020-01-05 02:15:12 -0500

    finished review page, added animation etc to meet mvp

--- src/components/TruckReview.js
+++ src/components/TruckReview.js
@@ -8,18 +8,34 @@ import {
   StyledTitleInput,
   StyledTruckReviewDiv,
   StyledTruckReviewFormDiv,
-  StyledTruckReviewMap
+  StyledTruckReviewMap,
+  StyledFab,
+  useStyles
 } from "../styles/TruckReviewStyles";
 import Rating from "@material-ui/lab/Rating";
 import Box from "@material-ui/core/Box";
+import { StylesProvider } from "@material-ui/styles";
+import clsx from "clsx";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import CheckIcon from "@material-ui/icons/Check";
+import Publish from "@material-ui/icons/Publish";
+import { useHistory } from "react-router-dom";
 
 const TruckReview = props => {
   // eslint-disable-next-line no-unused-vars
+  const classes = useStyles();
+  const [hover, setHover] = useState(0);
+  const { push } = useHistory();
+
   const [
     user,
     handlechange,
-    handlestarchange,
+    // eslint-disable-next-line no-unused-vars
     handlesubmit,
+    handleButtonClick,
+    handlestarchange,
+    success,
+    loading,
     value
   ] = useFormState(
     {
@@ -30,7 +46,10 @@ const TruckReview = props => {
     },
     props
   );
-  const [hover, setHover] = useState(0);
+
+  const buttonClassname = clsx({
+    [classes.buttonSuccess]: success
+  });
 
   const labels = {
     0: "Select your Rating",
@@ -47,7 +66,7 @@ const TruckReview = props => {
     <StyledTruckReviewDiv>
       <StyledTruckReviewFormDiv>
         <h1>&nbsp; Map Truck Title </h1>
-        <form onSubmit={handlesubmit}>
+        <form id='ReviewForm' onSubmit={handleButtonClick}>
           <fieldset style={{ border: `0` }} className='formfield'>
             <label className='forms'>
               <p
@@ -90,14 +109,44 @@ const TruckReview = props => {
                 value={user.review}
                 onChange={handlechange}></Inputtextarea>
             </StyledReviewDiv>
-
-            <StyledButton
-              className='ReviewButton'
-              submit={true}
-              size='large'
-              type='primary'>
-              Submit
-            </StyledButton>
+            <div className={classes.wrapper}>
+              <StylesProvider injectFirst>
+                <StyledButton
+                  variant='contained'
+                  color='default'
+                  type='submit'
+                  size='large'
+                  className={buttonClassname}
+                  disabled={loading}>
+                  Post Review
+                  {loading && (
+                    <CircularProgress
+                      size={32}
+                      className={classes.buttonProgress}
+                    />
+                  )}
+                </StyledButton>
+              </StylesProvider>
+              &nbsp;&nbsp;
+              <StylesProvider injectFirst>
+                <StyledFab
+                  aria-label='save'
+                  type='submit'
+                  color='primary'
+                  className={buttonClassname}>
+                  {success ? <CheckIcon /> : <Publish />}
+                  {/* Will Pushing Back to Truck Wall */}
+                  {success
+                    ? setTimeout(() => {
+                        push("/trucks/card");
+                      }, 1500)
+                    : null}
+                </StyledFab>
+                {loading && (
+                  <CircularProgress size={68} className={classes.fabProgress} />
+                )}
+              </StylesProvider>
+            </div>
           </fieldset>
         </form>
         {console.log(user, `user`)}
