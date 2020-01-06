43fad57a8171782186565c0b68a6faff1b8086b5
CalebAllard <allard.caleb@gmail.com>
2020-01-06 13:33:06 -0500

    asdf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 
 import React from 'react';
 import './App.css';
-
-
-
 import TruckReview from "./components/TruckReview";
 import Header from "./components/Header";
 import {connect} from "react-redux";
